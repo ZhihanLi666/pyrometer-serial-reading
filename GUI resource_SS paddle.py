14f65011665fe49dcb1d3a8bf89b62b2e91fc201
ZhihanLi666 <60561882+ZhihanLi666@users.noreply.github.com>
2024-03-13 14:47:45 -0400

    Delete GUI resource_SS paddle.py

--- GUI resource_SS paddle.py
+++
@@ -1,8 +0,0 @@
-#fitting and plotting file for SS paddle --GUI file source
-
-
-#Priliminary test with plot temp read from pyrometer via serial port
-
-
-
-#add fitting and plot based received photodiode current
